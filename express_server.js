b00bfbd258149eaccd1a96ec94f4d81fa722c8e1
unko-chan <kevinliang0303@gmail.com>
2021-01-20 03:15:00 -0800

    added testing templates and routes

--- express_server.js
+++ express_server.js
@@ -4,8 +4,18 @@ const PORT = 8080; //default port 8080
 
 app.set('view engine', 'ejs');
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+})
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
 const urlDatabase = {
-  b2xVn2: 'http://www.lighthouselabs.ca',
+  'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
