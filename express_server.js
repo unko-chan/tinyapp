4c7f6bb11a76354f4c74274f979ccfd5485bf4fe
unko-chan <kevinliang0303@gmail.com>
2021-01-20 04:29:54 -0800

    testing POST requests

--- express_server.js
+++ express_server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
-})
+});
+ 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
+  b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
@@ -39,6 +45,11 @@ app.get('/set', (req, res) => {
 app.get('/fetch', (req, res) => {
   res.send(`a = ${a}`);
 });
+
+app.post('urls/', (req, res) => {
+  console.log(req.body) // Log the POST request body to the console
+  res.send('Ok');       // Responds with ok  
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
