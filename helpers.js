e5bbf6298dc23d4f5022301df9ea14e23fdf6613
unko-chan <kevinliang0303@gmail.com>
2021-01-21 21:31:03 -0800

    seperate module for core functions

--- helpers.js
+++ helpers.js
@@ -1,9 +1,26 @@
+const bcrypt = require('bcrypt');
+
 const checkDupeEmail = function (email, database) {
   for (const key in database) {
     if (database[key].email === email) {
       return database[key].id;
-    } 
+    }
   }
 };
 
-module.exports = { checkDupeEmail }
+const generateRandomString = () => {
+  return Math.random().toString(20).substr(2, 6);
+};
+
+const checkCredentials = function (email, password, database) {
+  for (const key in database) {
+    if (
+      database[key].email === email &&
+      bcrypt.compareSync(password, database[key].password)
+    ) {
+      return key;
+    }
+  }
+};
+
+module.exports = { checkDupeEmail, generateRandomString, checkCredentials };
